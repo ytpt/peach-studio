0ba13e7f9330739cc279525f4802752a06198d6c
Anna Archive <ytpt@mail.ru>
2023-12-12 23:50:19 -0500

    Developed adaptive for tablet and mobile

--- src/data.js
+++ src/data.js
@@ -5,7 +5,9 @@ export const regions = {
                 id: 1,
                 title: "Москва",
                 fontSize: 14,
-                position: {x: 125, y: 215}
+                position: { x: 125, y: 215 },
+                tabletPosition: { x: 62, y: 114 },
+                mobilePosition: { x: 89, y: 154 },
             }
         ]
     },
@@ -15,19 +17,25 @@ export const regions = {
                 id: 1,
                 title: "Воронеж",
                 fontSize: 12,
-                position: { x: 77, y: 237 }
+                position: { x: 77, y: 237 },
+                tabletPosition: { x: 260, y: 110 },
+                mobilePosition: { x: 56, y: 171 },
             },
             {
                 id: 2,
                 title: "Ярославль",
                 fontSize: 12,
-                position: { x: 158, y: 188 }
+                position: { x: 158, y: 188 },
+                tabletPosition: { x: 330, y: 100 },
+                mobilePosition: { x: 115, y: 137 },
             },
             {
                 id: 3,
                 title: "Белгород",
                 fontSize: 12,
-                position: { x: 72, y: 268 }
+                position: { x: 72, y: 268 },
+                tabletPosition: { x: 260, y: 130 },
+                mobilePosition: { x: 49, y: 197 },
             },
         ]
     },
@@ -37,13 +45,17 @@ export const regions = {
                 id: 1,
                 title: "Санкт-Петербург",
                 fontSize: 16,
-                position: { x: 101, y: 123 }
+                position: { x: 101, y: 123 },
+                tabletPosition: { x: 18, y: 85 },
+                mobilePosition: { x: 75, y: 90 },
             },
             {
                 id: 2,
                 title: "Калининград",
                 fontSize: 12,
-                position: { x: 11, y: 116 }
+                position: { x: 11, y: 116 },
+                tabletPosition: { x: -5, y: 66 },
+                mobilePosition: { x: 8, y: 86 },
             },
         ]
     },
@@ -53,19 +65,25 @@ export const regions = {
                 id: 1,
                 title: "Ростов-на-Дону",
                 fontSize: 14,
-                position: { x: 42, y: 310 }
+                position: { x: 42, y: 310 },
+                tabletPosition: { x: 36, y: 170 },
+                mobilePosition: { x: 31, y: 223 },
             },
             {
                 id: 2,
                 title: "Волгоград",
                 fontSize: 12,
-                position: { x: 88, y: 352 }
+                position: { x: 88, y: 352 },
+                tabletPosition: { x: 70, y: 200 },
+                mobilePosition: { x: 64, y: 256 },
             },
             {
                 id: 3,
                 title: "Краснодар",
                 fontSize: 14,
-                position: { x: 24, y: 369 }
+                position: { x: 24, y: 369 },
+                tabletPosition: { x: 22, y: 200 },
+                mobilePosition: { x: 19, y: 267 },
             },
         ]
     },
@@ -75,31 +93,41 @@ export const regions = {
                 id: 1,
                 title: "Н. Новгород",
                 fontSize: 12,
-                position: { x: 167, y: 251 }
+                position: { x: 167, y: 251 },
+                tabletPosition: { x: 142, y: 110 },
+                mobilePosition: { x: 121, y: 183 },
             },
             {
                 id: 2,
                 title: "Самара",
                 fontSize: 12,
-                position: { x: 174, y: 298 }
+                position: { x: 174, y: 298 },
+                tabletPosition: { x: 152, y: 180 },
+                mobilePosition: { x: 127, y: 216 },
             },
             {
                 id: 3,
                 title: "Казань",
                 fontSize: 12,
-                position: { x: 237, y: 289 }
+                position: { x: 237, y: 289 },
+                tabletPosition: { x: 200, y: 150 },
+                mobilePosition: { x: 173, y: 210 },
             },
             {
                 id: 4,
                 title: "Оренбург",
                 fontSize: 12,
-                position: { x: 201, y: 356 }
+                position: { x: 201, y: 356 },
+                tabletPosition: { x: 180, y: 200 },
+                mobilePosition: { x: 147, y: 257 },
             },
             {
                 id: 5,
                 title: "Уфа",
                 fontSize: 12,
-                position: { x: 249, y: 327 }
+                position: { x: 249, y: 327 },
+                tabletPosition: { x: 200, y: 55 },
+                mobilePosition: { x: 181, y: 237 },
             },
         ]
     },
@@ -109,37 +137,49 @@ export const regions = {
                 id: 1,
                 title: "Ижевск",
                 fontSize: 12,
-                position: { x: 313, y: 280 }
+                position: { x: 313, y: 280 },
+                tabletPosition: { x: 265, y: 153 },
+                mobilePosition: { x: 227, y: 203 },
             },
             {
                 id: 2,
                 title: "Екатеринбург",
                 fontSize: 12,
-                position: { x: 292, y: 319 }
+                position: { x: 292, y: 319 },
+                tabletPosition: { x: 260, y: 180 },
+                mobilePosition: { x: 214, y: 232 },
             },
             {
                 id: 3,
                 title: "Челябинск",
                 fontSize: 14,
-                position: { x: 293, y: 352 }
+                position: { x: 293, y: 352 },
+                tabletPosition: { x: 270, y: 200 },
+                mobilePosition: { x: 213, y: 255 },
             },
             {
                 id: 4,
                 title: "Пермь",
                 fontSize: 14,
-                position: { x: 365, y: 290 }
+                position: { x: 365, y: 290 },
+                tabletPosition: { x: 300, y: 250 },
+                mobilePosition: { x: 265, y: 210 },
             },
             {
                 id: 5,
                 title: "Сургут",
                 fontSize: 12,
-                position: { x: 422, y: 303 }
+                position: { x: 422, y: 303 },
+                tabletPosition: { x: 370, y: 280 },
+                mobilePosition: { x: 300, y: 237 },
             },
             {
                 id: 5,
                 title: "Тюмень",
                 fontSize: 14,
-                position: { x: 395, y: 353 }
+                position: { x: 395, y: 353 },
+                tabletPosition: { x: 375, y: 152 },
+                mobilePosition: { x: 350, y: 350 },
             },
         ]
     },
@@ -149,31 +189,41 @@ export const regions = {
                 id: 1,
                 title: "Омск",
                 fontSize: 14,
-                position: { x: 449, y: 465 }
+                position: { x: 449, y: 465 },
+                tabletPosition: { x: 300, y: 210 },
+                mobilePosition: { x: 400, y: 360 },
             },
             {
                 id: 2,
                 title: "Новосибирск",
                 fontSize: 12,
-                position: { x: 484, y: 445 }
+                position: { x: 484, y: 445 },
+                tabletPosition: { x: 350, y: 250 },
+                mobilePosition: { x: 284, y: 300 },
             },
             {
                 id: 3,
                 title: "Томск",
                 fontSize: 14,
-                position: { x: 565, y: 449 }
+                position: { x: 565, y: 449 },
+                tabletPosition: { x: 320, y: 260 },
+                mobilePosition: { x: 326, y: 240 },
             },
             {
                 id: 4,
                 title: "Красноярск",
                 fontSize: 12,
-                position: { x: 610, y: 446 }
+                position: { x: 610, y: 446 },
+                tabletPosition: { x: 390, y: 260 },
+                mobilePosition: { x: 371, y: 237 },
             },
             {
                 id: 5,
                 title: "Иркутск",
                 fontSize: 14,
-                position: { x: 673, y: 479 }
+                position: { x: 673, y: 479 },
+                tabletPosition: { x: 450, y: 250 },
+                mobilePosition: { x: 399, y: 294 },
             },
         ]
     },
@@ -183,13 +233,17 @@ export const regions = {
                 id: 1,
                 title: "Хабаровск",
                 fontSize: 16,
-                position: { x: 955, y: 487 }
+                position: { x: 955, y: 487 },
+                tabletPosition: { x: 550, y: 250 },
+                mobilePosition: { x: 690, y: 287 },
             },
             {
                 id: 2,
                 title: "Владивосток",
                 fontSize: 16,
-                position: { x: 935, y: 565 }
+                position: { x: 935, y: 565 },
+                tabletPosition: { x: 550, y: 310 },
+                mobilePosition: { x: 671, y: 391 },
             },
         ]
     },
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import {regions} from "./data";
+import { regions } from './data';
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Toggle map menu buttons
+    const mapContainer = document.querySelector('.map__container');
+    const mapHeaderButtons = document.querySelectorAll('.map__header');
+    const mapHeader = document.querySelector('.map__header');
+    const arrowIcon = document.querySelector('.map__header-icon');
+    const mapImg = document.querySelector('.map__figure');
+    const hiddenMenu = document.querySelector('.map__menu--hidden');
+    const regionBlocks = document.querySelectorAll('.map__item--hidden');
     const mapButtons = document.querySelectorAll('.map__link');
+    const arrowAlt = arrowIcon.getAttribute('alt');
+    const sublistArray = document.querySelectorAll('.map__sublist');
+    const regionTitleArray = document.querySelectorAll('.map__title--hidden');
 
+    // Set attributes function
+    const setAttribute = (item, alt, path) => {
+        item.setAttribute('alt', alt);
+        item.setAttribute('src', path);
+    }
+
+    //Create triangle marker
+    const triangle = document.createElement('img');
+    setAttribute(triangle, 'Вниз','assets/triangle-down.svg');
+    triangle.classList.add('triangle');
+
+    // Toggle active menu button
     mapButtons.forEach(btn => {
         btn.addEventListener('click', function() {
             document.querySelector('.active').classList.remove('active');
@@ -11,48 +32,77 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Toggle hidden menu
-    const mapHeaderButton = document.querySelector('.map__header');
-    const arrowIcon = document.querySelector('.map__header-icon');
-    const mapImg = document.querySelector('.map__figure');
-    const hiddenMenu = document.querySelector('.map__menu--hidden');
+    // Toggle sublist
+    regionBlocks.forEach(region => {
+        region.addEventListener('click', function () {
+            const regionTitle = region.querySelector('.map__title--hidden');
+            const triangleItem = region.querySelector('.triangle');
+            const triangleAlt = triangleItem.getAttribute('alt');
 
-    mapHeaderButton.addEventListener('click', function() {
-        const currentAlt = arrowIcon.getAttribute('alt');
+            if (triangleAlt === 'Вниз') {
+                if (region.querySelector('.map__sublist')) {
+                    region.querySelector('.map__sublist').style.display = 'flex';
+                    setAttribute(triangleItem, 'Вверх', 'assets/triangle-up.svg');
+                    regionTitle.style.color = 'var(--color-red)';
+                }
+            } else {
+                if (region.querySelector('.map__sublist')) {
+                    region.querySelector('.map__sublist').style.display = 'none';
+                }
+                setAttribute(triangleItem, 'Вниз', 'assets/triangle-down.svg');
+                regionTitle.style.color = 'var(--color-dark)';
+            }
+        })
+    });
 
-        if (currentAlt === 'Вниз') {
-            mapButtons.forEach(btn => btn.classList.add('disable'));
-            mapImg.classList.add('disable');
-            hiddenMenu.style.maxHeight = '500px';
-            hiddenMenu.style.opacity = '1';
+    // Toggle hidden map menu
+    mapHeaderButtons.forEach(btn => {
+        btn.addEventListener('click', function() {
+            regionBlocks.forEach(region => {
 
-            arrowIcon.setAttribute('alt', 'Вверх');
-            arrowIcon.setAttribute('src', 'assets/arrow-up-black.svg');
-        } else {
-            mapButtons.forEach(btn => btn.classList.remove('disable'));
-            mapImg.classList.remove('disable');
-            hiddenMenu.style.maxHeight = '0';
-            hiddenMenu.style.opacity = '0';
+                // Add triangle markers
+                if (!region.querySelector('.triangle')) {
+                    const clonedTriangle = triangle.cloneNode(true);
+                    region.firstElementChild.append(clonedTriangle);
+                }
+            });
 
-            arrowIcon.setAttribute('alt', 'Вниз');
-            arrowIcon.setAttribute('src', 'assets/arrow-down-black.svg');
-        }
+            hiddenMenu.classList.toggle('visible');
+
+            if (arrowAlt === 'Вниз') {
+                mapButtons.forEach(btn => btn.classList.add('disable'));
+                mapImg.classList.add('disable');
+                setAttribute(arrowIcon, 'Вверх', 'assets/arrow-up-black.svg');
+                sublistArray.forEach(sublist => sublist.style.display = 'none');
+                regionTitleArray.forEach(title => title.style.color = 'var(--color-dark');
+            } else {
+                mapButtons.forEach(btn => btn.classList.remove('disable'));
+                mapImg.classList.remove('disable');
+                setAttribute(arrowIcon, 'Вниз', 'assets/arrow-down-black.svg');
+            }
+
+            if (window.innerWidth < 769) {
+                mapContainer.style.width = 'auto';
+            }
+        })
     })
 
     // Hide hidden menu while click outside menu
     document.addEventListener('click', function(event) {
         const isClickInsideMenu = hiddenMenu.contains(event.target);
-        const isClickOnHeaderButton = mapHeaderButton.contains(event.target);
+        const isClickOnHeaderButton = mapHeader.contains(event.target);
 
         if (!isClickInsideMenu && !isClickOnHeaderButton) {
-            hiddenMenu.style.maxHeight = '0';
-            hiddenMenu.style.opacity = '0';
-            arrowIcon.setAttribute('alt', 'Вниз');
-            arrowIcon.setAttribute('src', 'assets/arrow-down-black.svg');
+            if (hiddenMenu.classList.contains('visible')) {
+                hiddenMenu.classList.toggle('visible');
+            }
+
+            setAttribute(arrowIcon, 'Вниз', 'assets/arrow-down-black.svg');
             mapButtons.forEach(btn => btn.classList.remove('disable'));
             mapImg.classList.remove('disable');
         }
     });
+
 
     // Toggle map cities marks
     const mapFigure = document.querySelector('.map__figure');
@@ -90,20 +140,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function drawMapPoints(citiesInRegion) {
         citiesInRegion.cities.forEach(city => {
-            const cityCoords = city.position;
+            let cityCoords;
             const cityName = city.title;
             const fontSize = city.fontSize;
 
             let point = document.createElement('div');
             point.classList.add('mark');
+
+            switch (true) {
+                case window.innerWidth < 768:
+                    cityCoords = city.mobilePosition;
+                    break;
+                case window.innerWidth >= 768 && window.innerWidth < 1289:
+                    cityCoords = city.tabletPosition;
+                    break;
+                default:
+                    cityCoords = city.position;
+                    break;
+            }
+
             point.style.left = `${cityCoords.x}px`;
             point.style.top = `${cityCoords.y}px`;
 
             let pointImg = document.createElement('img');
-            pointImg.src = 'assets/ellipse.svg';
-            pointImg.alt = 'Точка';
-            pointImg.width = 8;
-            pointImg.height = 8;
+            setAttribute(pointImg, 'Точка', 'assets/ellipse.svg');
+            pointImg.classList.add('dot-size');
 
             let pointTitle = document.createElement('p');
             pointTitle.classList.add('mark__title');
@@ -123,31 +184,34 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Directions items hover effect
-    function addHoverEffect() {
-        const directionsItems = document.querySelectorAll('.directions__item');
+    /* Toggle directions items by click */
+    if (window.innerWidth < 769) {
+        const items = document.querySelectorAll('.directions__item');
 
-        if (document.documentElement.clientWidth > 1200) {
-            directionsItems.forEach(item => {
-                item.addEventListener('mouseover', function() {
-                    item.querySelector('.directions__item-text').style.display = 'block';
-                    item.querySelector('.directions__item-block').classList.remove('regular');
-                    item.querySelector('.directions__item-block').classList.add('hover');
-                });
+        items.forEach(item => {
+            const itemBlock = item.querySelector('.directions__item-block');
+            const itemText = item.querySelector('.directions__item-text');
+            const arrowIcon = item.querySelector('.directions__item-icon');
 
-                item.addEventListener('mouseout', function() {
-                    item.querySelector('.directions__item-text').style.display = 'none';
-                    item.querySelector('.directions__item-block').classList.remove('hover');
-                    item.querySelector('.directions__item-block').classList.add('regular');
-                });
-            });
-        }
+            item.addEventListener('click', function() {
+                const currentAlt = arrowIcon.getAttribute('alt');
+                if (currentAlt === 'Вниз') {
+                    itemBlock.classList.toggle('block-visible');
+                    item.classList.toggle('item-expand');
+                    itemText.style.display = 'block';
+                    setAttribute(arrowIcon, 'Вверх','assets/arrow-up-white.svg');
+                } else {
+                    item.classList.toggle('item-expand');
+                    itemBlock.classList.toggle('block-visible');
+                    itemText.style.display = 'none';
+                    setAttribute(arrowIcon, 'Вниз','assets/arrow-down-white.svg');
+                }
+            })
+        })
     }
-    addHoverEffect();
-    window.addEventListener('resize', addHoverEffect);
 
     /* Slider */
-    const swiper = new Swiper('.swiper-container', {
+    new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 10,
         loop: true,
